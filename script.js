41ff05a5d746a00399cf75b66b863da0070833a9
ChaitraSuvarnaa <chaitrasuvarna4065@gmail.com>
2024-06-21 16:20:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function convertTemperature() {
+    let celsius = document.getElementById('celsius').value;
+    let result = document.getElementById('result');
+
+    if (celsius === "") {
+        result.textContent = "";
+    } else {
+        let fahrenheit = (celsius * 9 / 5) + 32;
+        fahrenheit = fahrenheit.toFixed(2);
+        result.textContent = `Fahrenheit: ${fahrenheit}`;
+    }
+}
+
+document.querySelector('button').addEventListener('click', convertTemperature);
